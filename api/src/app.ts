ad775165748bdbc86120890d9cc78a4af430170e
Majid Joseph <majid.joseph7@gmail.com>
2021-04-27 23:46:59 -0400

    connecting mongo

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import passport from "passport";
+import {connectMongoDB} from "./config/mongoose";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.API_PORT || 8080;
+const mongoUrl = process.env.MONGO_URL as string;
+
+connectMongoDB(mongoUrl);
 
 app.set("port", PORT);
 
 app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.json());
 
-app.get("/", (req, res)  => {
+app.get("/", (req, res) => {
     res.send("OK");
 })
 
