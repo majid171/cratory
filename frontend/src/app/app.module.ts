0292e1d83e0d538b120c5412066489f392484f40
Majid Joseph <majid.joseph7@gmail.com>
2021-05-14 00:50:34 -0400

    styling login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HomeComponent } from './components/home/home.component';
+import { ButtonComponent } from './components/common/button/button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+// import {  } from '@angular/material/l';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +25,17 @@ import { HomeComponent } from './components/home/home.component';
     LoginComponent,
     PageNotFoundComponent,
     HomeComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
