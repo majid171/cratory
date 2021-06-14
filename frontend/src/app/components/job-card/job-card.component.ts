7a64d501510f85a1a49bb33c7add19a525c05bee
Majid Joseph <majid.joseph7@gmail.com>
2021-06-14 00:26:18 -0400

    styling job component

--- frontend/src/app/components/job-card/job-card.component.ts
+++ frontend/src/app/components/job-card/job-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-job-card',
@@ -7,14 +7,51 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class JobCardComponent implements OnInit {
 
-  @Input() title: String;
-  @Input() description: String;
-  @Input() price: Number;
-  @Input() skills: String[];
-  @Input() issuer: String;
+  @Input() title: string;
+  @Input() description: string;
+  @Input() price: number;
+  @Input() skills: string[];
+  @Input() issuer: string;
+  @Input() location: string;
 
+  amountOfWordsToDisplay: number = 200;
+  showCompressedSkills: boolean;
+  showSmallPrice: boolean;
+  numberOfExtraSkills: number;
+  skillsCopy: string[];
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.skillsCopy = this.skills;
+    this.numberOfExtraSkills = this.skills.length - 2;
+    this.canShowCompressedLists();
+    this.canShowSmallPrice();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.canShowCompressedLists();
+    this.canShowSmallPrice();
+  }
+
+  canShowCompressedLists(): void {
+    if (window.screen.width < 769 && this.skills.length > 2) {
+      this.showCompressedSkills = true;
+      this.skillsCopy = this.skillsCopy.slice(0, 2);
+    }
+    else{
+      this.showCompressedSkills = false;
+      this.skillsCopy = this.skills;
+    }
+  }
+
+  canShowSmallPrice(): void {
+    if(window.screen.width <= 1024){
+      this.showSmallPrice = true;
+    }
+    else{
+      this.showSmallPrice = false;
+    }
   }
 }
